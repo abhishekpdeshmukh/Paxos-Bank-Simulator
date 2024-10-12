88416d9078da24c00cc7644824508bb679092b4c
abhishekpdeshmukh <38159363+abhishekpdeshmukh@users.noreply.github.com>
2024-10-12 00:05:38 -0400

    Initial implementation of transaction commit and insertion into SQLite DB

--- node/node.go
+++ node/node.go
@@ -2,66 +2,51 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
 
 	pb "github.com/abhishekpdeshmukh/PAXOS-BANK-SIMULATOR/proto"
+	_ "github.com/mattn/go-sqlite3"
 	"google.golang.org/grpc"
 )
 
 type Node struct {
 	pb.UnimplementedNodeServiceServer
 	pb.UnimplementedPaxosServiceServer
-	nodeID               int32
-	isActive             bool
-	myBalance            int32
-	currBallotNum        int32
-	isPromised           bool
-	promisedBallot       *pb.Ballot
-	AcceptedTransactions []*pb.TransactionRequest
-	transactionLog       []*pb.TransactionRequest
-	megaBlock            []*pb.TransactionRequest
-	lock                 sync.Locker
+	nodeID            int32
+	isActive          bool
+	myBalance         int32
+	currBallotNum     int32
+	isPromised        bool
+	promisedBallot    *pb.Ballot
+	transactionLog    []*pb.TransactionRequest
+	megaBlock         []*pb.TransactionRequest
+	AcceptedMegaBlock []*pb.TransactionRequest
+	db                *sql.DB
+	lock              sync.Mutex
 }
-
-//	type Ballot struct {
-//		nodeID    int
-//		ballotNum int
-//	}
-type Transaction struct {
-	SetNumber int
-	From      int
-	To        int
-	Amount    int
-}
-
-// var transactions []Transaction
 
 func (s *Node) AcceptTransactions(ctx context.Context, req *pb.TransactionRequest) (*pb.NodeResponse, error) {
-	// x := pb.TransactionRequest{
-	// 	SetNumber: req.SetNumber),
-	// 	From:      req.From),
-	// 	To:        req.To),
-	// 	Amount:    req.Amount),
-	// }
+
 	fmt.Println("Trying to Accept Transactions")
 
 	if s.myBalance-int32(req.Amount) < 0 {
 		fmt.Println("Initiate consensus")
 		var wg sync.WaitGroup
 		wg.Add(4)
+		s.currBallotNum += 1
 		for i := 1; i < 5; i++ {
-			go func(i int) {
-				defer wg.Done()
+			go func(i int, s *Node) {
 				if i != int(s.nodeID) {
 					c, ctx, conn := SetupNodeRpcReciever(i)
-					s.currBallotNum += 1
 					promise, err := c.Prepare(ctx, &pb.PrepareRequest{
 						Ballot: &pb.Ballot{
 							BallotNum: s.currBallotNum,
@@ -71,7 +56,8 @@ func (s *Node) AcceptTransactions(ctx context.Context, req *pb.TransactionReques
 					if err != nil {
 						log.Fatalf("Could not add: %v", err)
 					}
-					// fmt.Println(promise)
+					fmt.Println("Promise From ", i)
+					fmt.Println(promise)
 					s.lock.Lock()
 					for _, transaction := range promise.Accept_Val {
 						if transaction.To == s.nodeID {
@@ -87,16 +73,20 @@ func (s *Node) AcceptTransactions(ctx context.Context, req *pb.TransactionReques
 					fmt.Println(promise)
 					s.megaBlock = append(s.megaBlock, promise.Accept_Val...)
 					s.megaBlock = append(s.megaBlock, promise.Local...)
-					s.megaBlock = append(s.megaBlock, s.transactionLog...)
-					s.megaBlock = append(s.megaBlock, req)
 					s.lock.Unlock()
 					conn.Close()
 				}
-			}(i)
+				defer wg.Done()
+			}(i, s)
 		}
 		wg.Wait()
 		wg.Add(4)
+		s.lock.Lock()
+		s.megaBlock = append(s.megaBlock, s.transactionLog...)
+		s.megaBlock = append(s.megaBlock, req)
+
 		fmt.Println("MY MEGA BLOCK IS ")
+
 		fmt.Println(s.megaBlock)
 		for i := 1; i < 5; i++ {
 			go func(i int) {
@@ -117,13 +107,45 @@ func (s *Node) AcceptTransactions(ctx context.Context, req *pb.TransactionReques
 			}(i)
 		}
 		wg.Wait()
+		wg.Add(4)
+		for i := 1; i < 5; i++ {
+			go func(i int) {
+				defer wg.Done()
+				if i != int(s.nodeID) {
+
+					c, ctx, conn := SetupNodeRpcReciever(i)
+					ack, err := c.Commit(ctx, &pb.CommitRequest{
+						BallotNumber: &pb.Ballot{
+							BallotNum: s.currBallotNum,
+							NodeID:    s.nodeID,
+						},
+					})
+					if err != nil {
+						log.Fatalf("Could not add: %v", err)
+					}
+
+					fmt.Println(ack)
+					conn.Close()
+				}
+			}(i)
+		}
+		wg.Wait()
+		err := s.commitTransactions(true) // Commit transactions to the database
+		if err != nil {
+			return nil, err
+		}
+		s.lock.Unlock()
 	}
 	s.lock.Lock()
 	if s.myBalance-int32(req.Amount) >= 0 {
 		s.myBalance -= req.Amount
 		s.transactionLog = append(s.transactionLog, req)
+		// if len(s.megaBlock) != 0 {
+
+		// }
 		fmt.Println("This my transaction log ", s.transactionLog)
 	}
+
 	s.lock.Unlock()
 	return &pb.NodeResponse{Ack: "Node " + strconv.Itoa(int(s.nodeID)) + " Took All transactions"}, nil
 }
@@ -151,28 +173,27 @@ func (s *Node) GetBalance(ctx context.Context, req *pb.AdminRequest) (*pb.Balanc
 func (s *Node) Prepare(ctx context.Context, req *pb.PrepareRequest) (*pb.PromiseResponse, error) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-
-	// if s.isPromised && compareBallot(s, req) {
-	fmt.Println("AcceptedTransactions: ", s.AcceptedTransactions)
+	fmt.Println("AcceptedTransactions: ", s.AcceptedMegaBlock)
 	fmt.Println("TransactionLog: ", s.transactionLog)
-
 	s.promisedBallot = req.Ballot
-	return &pb.PromiseResponse{
-		BallotNumber: req.Ballot,
-		AcceptNum:    s.promisedBallot,
-		Accept_Val:   s.AcceptedTransactions,
-		Local:        s.transactionLog,
-	}, nil
-
-	// } else if !s.isPromised && (s.currBallotNum < req.Ballot.BallotNum || s.currBallotNum == req.Ballot.BallotNum && s.nodeID < req.Ballot.NodeID) {
-	// 	return &pb.PromiseResponse{
-	// 		BallotNumber: req.Ballot, // Promise on the received ballot number
-	// 		AcceptNum:    nil,        // No previously accepted ballot
-	// 		Accept_Val:   nil,        // No previously accepted values
-	// 		Local:        s.transactionLog,
-	// 	}, nil
-	// }
-	// return &pb.PromiseResponse{}, fmt.Errorf("Prepare request rejected; higher or equal ballot number exists")
+	if s.isPromised && compareBallot(s, req) {
+		s.isPromised = true
+		return &pb.PromiseResponse{
+			BallotNumber: req.Ballot,
+			AcceptNum:    s.promisedBallot,
+			Accept_Val:   s.AcceptedMegaBlock,
+			Local:        s.transactionLog,
+		}, nil
+	} else if !s.isPromised && (s.currBallotNum < req.Ballot.BallotNum || s.currBallotNum == req.Ballot.BallotNum && s.nodeID < req.Ballot.NodeID) {
+		s.isPromised = true
+		return &pb.PromiseResponse{
+			BallotNumber: req.Ballot, // Promise on the received ballot number
+			AcceptNum:    nil,        // No previously accepted ballot
+			Accept_Val:   nil,        // No previously accepted values
+			Local:        s.transactionLog,
+		}, nil
+	}
+	return &pb.PromiseResponse{}, fmt.Errorf("Prepare request rejected; higher or equal ballot number exists")
 
 }
 
@@ -180,37 +201,128 @@ func (s *Node) Accept(ctx context.Context, req *pb.AcceptRequest) (*pb.AcceptedR
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	fmt.Println("Inside Accept")
-	// if req.ProposalNumber >= s.currBallotNum {
-	fmt.Println(req.Value)
-	for _, transaction := range req.Value {
+	s.AcceptedMegaBlock = req.Value
+	fmt.Println("Accepted Mega Block: ", s.AcceptedMegaBlock)
+	fmt.Println("TransactionLog: ", s.transactionLog)
+	if req.ProposalNumber >= s.currBallotNum {
+
+		return &pb.AcceptedResponse{ProposalNumber: req.ProposalNumber}, nil
+	}
+	return &pb.AcceptedResponse{}, nil
+}
+
+func (s *Node) Commit(ctx context.Context, req *pb.CommitRequest) (*pb.CommitedResponse, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	fmt.Println("Inside Commit")
+	s.sortTransactions()
+	for _, transaction := range s.AcceptedMegaBlock {
 		if transaction.To == s.nodeID {
 			s.myBalance += transaction.Amount
 		}
 	}
-	return &pb.AcceptedResponse{ProposalNumber: req.ProposalNumber}, nil
-	// }
-	// return &pb.AcceptedResponse{}, nil
+	if s.promisedBallot.BallotNum == req.BallotNumber.BallotNum {
+		err := s.commitTransactions(false) // Commit transactions to the database
+		if err != nil {
+			return nil, err
+		}
+		return &pb.CommitedResponse{
+			Success: true,
+		}, nil
+	}
+	return &pb.CommitedResponse{
+		Success: false,
+	}, nil
+}
+
+func (s *Node) InitDB() {
+	db, err := sql.Open("sqlite3", fmt.Sprintf("node_%d.db", s.nodeID))
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	s.db = db
+	// Create a transactions table if it doesn't exist
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY,
+        set_number INTEGER,
+        from_account INTEGER,
+        to_account INTEGER,
+        amount INTEGER
+    )`)
+	if err != nil {
+		fmt.Println("Error creating table:", err)
+	}
+}
+
+func (s *Node) sortTransactions() {
+	sort.Slice(s.AcceptedMegaBlock, func(i, j int) bool {
+		return s.AcceptedMegaBlock[i].Id < s.AcceptedMegaBlock[j].Id
+	})
+}
+func (s *Node) commitTransactions(leader bool) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(`INSERT INTO transactions (id, set_number, from_account, to_account, amount)
+                             VALUES (?, ?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	if !leader {
+		for _, transaction := range s.AcceptedMegaBlock {
+			_, err := stmt.Exec(transaction.Id, transaction.SetNumber, transaction.From, transaction.To, transaction.Amount)
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+		}
+	} else {
+		for _, transaction := range s.megaBlock {
+			_, err := stmt.Exec(transaction.Id, transaction.SetNumber, transaction.From, transaction.To, transaction.Amount)
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+			fmt.Println(transaction)
+		}
+	}
+
+	return tx.Commit()
 }
 
 func main() {
 	id, _ := strconv.Atoi(os.Args[1])
 
-	currNode := Node{nodeID: int32(id), isActive: true, myBalance: 200, currBallotNum: 0, promisedBallot: &pb.Ballot{}, lock: new(sync.Mutex)}
+	currNode := &Node{
+		nodeID:         int32(id),
+		isActive:       true,
+		myBalance:      200,
+		currBallotNum:  0,
+		promisedBallot: &pb.Ballot{},
+		lock:           sync.Mutex{},
+	}
+	currNode.InitDB()
 	go SetupRpc(id, currNode)
 	go SetupNodeRpcSender(id, currNode)
+
 	println("Node ", id, " Working")
 	for {
 
 	}
 }
 
-func SetupNodeRpcSender(id int, node Node) {
+func SetupNodeRpcSender(id int, node *Node) {
 	lis, err := net.Listen("tcp", ":5005"+strconv.Itoa(id+5))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterPaxosServiceServer(s, &node)
+	pb.RegisterPaxosServiceServer(s, node)
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
@@ -231,13 +343,13 @@ func SetupNodeRpcReciever(id int) (pb.PaxosServiceClient, context.Context, *grpc
 	return c, ctx, conn
 }
 
-func SetupRpc(id int, node Node) {
+func SetupRpc(id int, node *Node) {
 	lis, err := net.Listen("tcp", ":5005"+strconv.Itoa(id))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterNodeServiceServer(s, &node)
+	pb.RegisterNodeServiceServer(s, node)
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
